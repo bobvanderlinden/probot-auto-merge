3c44f1153175f50df25635b41dc6403c9e2f601b
Bob van der Linden <bobvanderlinden@gmail.com>
2018-07-12 21:38:14 +0200

    add repo/owner #pr to logs

--- src/index.ts
+++ src/index.ts
@@ -44,18 +44,22 @@ export = (app: Application) => {
     const owner = pullRequest.base.user.login
     const number = pullRequest.number
 
+    function log(msg: string) {
+      app.log(`${repo}/${owner} #${number}: ${msg}`)
+    }
+
     if (pullRequest.state !== 'open') {
-      app.log('Pull request not open')
+      log('Pull request not open')
       return
     }
 
     if (pullRequest.merged) {
-      app.log('Pull request was already merged')
+      log('Pull request was already merged')
       return
     }
 
     if (!pullRequest.mergeable) {
-      app.log('Pull request is not mergeable: ' + pullRequest.mergeable)
+      log('Pull request is not mergeable: ' + pullRequest.mergeable)
       return
     }
 
@@ -74,29 +78,29 @@ export = (app: Application) => {
       .map(([user, state]) => `${user}: ${state}`)
       .join('\n')
 
-    app.log(`\nReviews:\n${reviewSummary}\n\n`)
+    log(`\nReviews:\n${reviewSummary}\n\n`)
 
     const reviewStates = Object.values(latestReviews)
     const changesRequestedCount = reviewStates.filter(reviewState => reviewState === 'CHANGES_REQUESTED').length
     const approvalCount = reviewStates.filter(reviewState => reviewState === 'APPROVED').length
     if (changesRequestedCount > 0) {
-      app.log('There are changes requested by a reviewer')
+      log(`There are changes requested by a reviewer (${changesRequestedCount} / ${config["min-approvals"]})`)
       return
     }
     if (approvalCount < 1) {
-      app.log('There are not enough approvals by reviewers')
+      log(`There are not enough approvals by reviewers (${approvalCount} / ${config["max-requested-changes"]})`)
       return
     }
     const checksResponse = await context.github.checks.listForRef({owner, repo, ref: pullRequest.head.sha, filter: 'latest' })
     const checkRuns: CheckRun[] = checksResponse.data.check_runs
-    // app.log('checks: ' + JSON.stringify(checks))
+    // log('checks: ' + JSON.stringify(checks))
     const checksSummary = checkRuns.map(checkRun => `${checkRun.name}: ${checkRun.status}: ${checkRun.conclusion}`).join('\n')
 
-    app.log(`\nChecks:\n${checksSummary}\n\n`)
+    log(`\nChecks:\n${checksSummary}\n\n`)
 
     const allChecksCompleted = checkRuns.every(checkRun => checkRun.status === 'completed')
     if (!allChecksCompleted) {
-      app.log(`There are still pending checks. Scheduling recheck.`)
+      log(`There are still pending checks. Scheduling recheck.`)
       setTimeout(async () => {
         await updatePullRequest(context, pullRequest)
       }, 60000)
@@ -105,14 +109,14 @@ export = (app: Application) => {
     const checkConclusions: {
       [conclusion: string]: boolean
     } = checkRuns.reduce((result, checkRun) => ({ ...result, [checkRun.conclusion]: true }), {})
-    app.log('conclusions: ' + JSON.stringify(checkConclusions))
+    log('conclusions: ' + JSON.stringify(checkConclusions))
     const checksBlocking = checkConclusions.failure || checkConclusions.cancelled || checkConclusions.timed_out || checkConclusions.action_required
     if (checksBlocking) {
-      app.log(`There are blocking checks`)
+      log(`There are blocking checks`)
       return
     }
     await context.github.pullRequests.merge({owner, repo, number, merge_method: 'merge'})
     // await context.github.issues.createComment({owner, repo, number, body: 'I want to merge this right now'})
-    app.log('Merge pull request')
+    log('Merge pull request')
   }
 }
