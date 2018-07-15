55c67c0f5ac729df7742687a580e11802ab8242d
Bob van der Linden <bobvanderlinden@gmail.com>
2018-07-15 13:47:37 +0200

    add test for updating branch and removing branch

--- test/pull-request-handler.test.ts
+++ test/pull-request-handler.test.ts
@@ -49,4 +49,158 @@ describe("handlePullRequestStatus", () => {
     }
     expect(setTimeout).toHaveBeenCalledTimes(0);
   });
+  it("update branch when status is out_of_date_branch", async () => {
+    const merge = githubCallMock(null);
+    const { context, pullRequestInfo } = mockPullRequestContext({
+      config: {
+        "update-branch": true
+      },
+      githubRepoMerge: merge
+    });
+    await handlePullRequestStatus(context, pullRequestInfo, {
+      code: "out_of_date_branch",
+      message: "bogus",
+      merge: {
+        base: "mybranch",
+        head: "master",
+        owner: "john",
+        repo: "test"
+      }
+    });
+    expect(merge).toHaveBeenCalledTimes(1);
+    expect(merge.mock.calls[0]).toEqual([
+      {
+        base: "mybranch",
+        head: "master",
+        owner: "john",
+        repo: "test"
+      }
+    ]);
+  });
+  it("update branch when status is out_of_date_branch and update-branch is enabled", async () => {
+    const mergeParameters = {
+      base: "mybranch",
+      head: "master",
+      owner: "john",
+      repo: "test"
+    };
+    const merge = githubCallMock(null);
+    const { context, pullRequestInfo } = mockPullRequestContext({
+      config: {
+        "update-branch": true
+      },
+      githubRepoMerge: merge
+    });
+    await handlePullRequestStatus(context, pullRequestInfo, {
+      code: "out_of_date_branch",
+      message: "bogus",
+      merge: mergeParameters
+    });
+    expect(merge).toHaveBeenCalledTimes(1);
+    expect(merge.mock.calls[0]).toEqual([mergeParameters]);
+  });
+  it("not update branch when status is out_of_date_branch and update-branch is disabled", async () => {
+    const mergeParameters = {
+      base: "mybranch",
+      head: "master",
+      owner: "john",
+      repo: "test"
+    };
+    const merge = githubCallMock(null);
+    const { context, pullRequestInfo } = mockPullRequestContext({
+      config: {
+        "update-branch": false
+      },
+      githubRepoMerge: merge
+    });
+    await handlePullRequestStatus(context, pullRequestInfo, {
+      code: "out_of_date_branch",
+      message: "bogus",
+      merge: mergeParameters
+    });
+    expect(merge).toHaveBeenCalledTimes(0);
+  });
+  it("delete branch when status is ready_for_merge and delete-branch-after-merge is enabled and branch resides in same repository", async () => {
+    const githubGitdataDeleteReference = githubCallMock(null);
+    const { context, pullRequestInfo } = mockPullRequestContext({
+      config: {
+        "delete-branch-after-merge": true
+      },
+      pullRequest: {
+        base: {
+          ref: "master",
+          sha: "12345",
+          user: {
+            login: "john"
+          },
+          repo: {
+            name: "test"
+          }
+        },
+        head: {
+          ref: "mybranch",
+          sha: "56789",
+          user: {
+            login: "john"
+          },
+          repo: {
+            name: "test"
+          }
+        }
+      },
+      pullRequestInfo: {
+        owner: "john",
+        repo: "test"
+      },
+      githubGitdataDeleteReference
+    });
+    await handlePullRequestStatus(context, pullRequestInfo, {
+      code: "ready_for_merge",
+      message: "bogus"
+    });
+    expect(githubGitdataDeleteReference).toHaveBeenCalledTimes(1);
+    expect(githubGitdataDeleteReference.mock.calls[0]).toEqual([
+      { owner: "john", ref: "heads/mybranch", repo: "test" }
+    ]);
+  });
+  it("do not delete branch when status is ready_for_merge and delete-branch-after-merge is enabled, but branch resides in another repository", async () => {
+    const githubGitdataDeleteReference = githubCallMock(null);
+    const { context, pullRequestInfo } = mockPullRequestContext({
+      config: {
+        "delete-branch-after-merge": true
+      },
+      pullRequest: {
+        base: {
+          ref: "master",
+          sha: "12345",
+          user: {
+            login: "john"
+          },
+          repo: {
+            name: "test"
+          }
+        },
+        head: {
+          ref: "mybranch",
+          sha: "56789",
+          user: {
+            login: "hank"
+          },
+          repo: {
+            name: "test"
+          }
+        }
+      },
+      pullRequestInfo: {
+        owner: "john",
+        repo: "test"
+      },
+      githubGitdataDeleteReference
+    });
+    await handlePullRequestStatus(context, pullRequestInfo, {
+      code: "ready_for_merge",
+      message: "bogus"
+    });
+    expect(githubGitdataDeleteReference).toHaveBeenCalledTimes(0);
+  });
 });
