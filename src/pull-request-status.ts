61f9eee18a0fa60a8bb08ad238103b01335d5634
Bob van der Linden <bobvanderlinden@gmail.com>
2018-08-18 20:52:40 +0200

    split status conditions into separate functions

--- src/pull-request-status.ts
+++ src/pull-request-status.ts
@@ -1,5 +1,7 @@
+import { HandlerContext, PullRequestInfo, PullRequestInfo } from './../lib/src/models.d';
+import { PullRequestInfo, PullRequestInfo, CommentAuthorAssociation, PullRequestReviewState } from './github-models';
 import { HandlerContext, PullRequestInfo } from './models'
-import { groupByLast, groupByLastMap } from './utils'
+import { groupByLast, groupByLastMap, result } from './utils'
 import { associations, getAssociationPriority } from './association'
 
 export type PullRequestStatus =
@@ -37,90 +39,142 @@ export const PullRequestStatusCodes: PullRequestStatusCode[] = [
   'ready_for_merge'
 ]
 
-function getPullRequestStatusFromPullRequest (
-  context: HandlerContext,
-  pullRequestInfo: PullRequestInfo
-): PullRequestStatus | null {
-  switch (pullRequestInfo.state) {
-    case 'CLOSED':
-      return {
-        code: 'closed',
-        message: 'Pull request is closed'
-      }
-    case 'MERGED':
-      return {
-        code: 'merged',
-        message: 'Pull request was already merged'
-      }
-    default:
-      return {
-        code: 'not_open',
-        message: 'Pull request is not open'
-      }
-    case 'OPEN':
-      // Continue.
-      break
-  }
+const conditions = {
+  open: isOpen,
+  mergeable: isMergeable,
+  requiredLabels: hasRequiredLabels,
+  blockingLabels: doesNotHaveBlockingLabels,
+  minimumApprovals: hasMinimumApprovals,
+  maximumChangesRequested: doesNotHaveMaximumChangesRequested,
+  blockingChecks: doesNotHaveBlockingChecks,
+  upToDateBranch: hasUpToDateBranch
+}
 
+type Condition = (context: HandlerContext, pullRequestInfo: PullRequestInfo) => ConditionResult
+type ConditionName = keyof (typeof conditions)
+type ConditionResult = { status: 'success' } | { status: 'fail', message: string } | { status: 'pending', message?: string }
+type ConditionResults = { [key in ConditionName]: ConditionResult }
+
+function isOpen (context: HandlerContext, pullRequestInfo: PullRequestInfo): ConditionResult {
+  return pullRequestInfo.state === 'OPEN'
+    ? {
+      status: 'success'
+    }
+    : {
+      status: 'fail',
+      message: `State of pull request is ${pullRequestInfo.state}`
+    }
+}
+
+function isMergeable (context: HandlerContext, pullRequestInfo: PullRequestInfo): ConditionResult {
   switch (pullRequestInfo.mergeable) {
+    case 'MERGEABLE':
+      return {
+        status: 'success'
+      }
     case 'CONFLICTING':
       return {
-        code: 'conflicts',
-        message: 'Could not merge pull request due to conflicts'
+        status: 'fail',
+        message: 'Pull request has conflicts'
       }
     case 'UNKNOWN':
       return {
-        code: 'pending_mergeable',
-        message: 'Mergeablity of pull request could not yet be determined'
+        status: 'pending',
+        message: 'Github could not yet determine the mergeable status of the pull request'
       }
-    case 'MERGEABLE':
-      // Continue.
-      break
+    default:
+      throw new Error(`Invalid mergeable state for pull request. mergeable=${pullRequestInfo.mergeable}`)
   }
-  return null
 }
 
-function getPullRequestStatusFromLabels (
+function hasRequiredLabels (
   context: HandlerContext,
   pullRequestInfo: PullRequestInfo
-): PullRequestStatus | null {
+): ConditionResult {
   const { config } = context
-  function hasLabel (labelName: string): boolean {
-    return pullRequestInfo.labels.nodes.some(label => label.name === labelName)
-  }
+  const pullRequestLabels = new Set(pullRequestInfo.labels.nodes.map(label => label.name))
 
-  const missingRequiredLabels = config.requiredLabels.filter(
-    requiredLabel => !hasLabel(requiredLabel)
-  )
+  const missingRequiredLabels = config.requiredLabels
+    .filter(requiredLabel => !pullRequestLabels.has(requiredLabel))
+
   if (missingRequiredLabels.length > 0) {
     return {
-      code: 'requires_label',
-      message: `Required labels are missing (${missingRequiredLabels.join(
-        ', '
-      )})`
-    }
-  }
-
-  const matchingBlockingLabels = config.blockingLabels.filter(
-    blockingLabel => hasLabel(blockingLabel)
-  )
-  if (matchingBlockingLabels.length > 0) {
-    return {
-      code: 'blocking_label',
-      message: `Blocking labels were added to the pull request (${
-        matchingBlockingLabels.join(', ')
+      status: 'fail',
+      message: `Required labels are missing (${
+        missingRequiredLabels.join(', ')
       })`
     }
   }
-
-  return null
+  return {
+    status: 'success'
+  }
 }
 
-function getPullRequestStatusFromReviews (
+function doesNotHaveBlockingLabels (
   context: HandlerContext,
   pullRequestInfo: PullRequestInfo
-): PullRequestStatus | null {
-  const { config, log } = context
+): ConditionResult {
+  const { config } = context
+  const pullRequestLabels = new Set(pullRequestInfo.labels.nodes.map(label => label.name))
+  const foundBlockingLabels = config.blockingLabels
+    .filter(blockingLabel => pullRequestLabels.has(blockingLabel))
+
+  if (foundBlockingLabels.length > 0) {
+    return {
+      status: 'fail',
+      message: `Blocking labels are missing (${
+        foundBlockingLabels.join(', ')
+      })`
+    }
+  }
+  return {
+    status: 'success'
+  }
+}
+
+function merge<A, B> (a: A, b: B): A & B {
+  return Object.assign({}, a, b) as any
+}
+
+function arrayToMap<TKey extends string, TValue, TItem> (
+  arr: Array<TItem>,
+  keyFn: (item: TItem) => TKey,
+  valueFn: (item: TItem) => TValue
+): { [key in TKey]?: TValue } {
+  return arr.reduce((result: { [key in TKey]?: TValue }, item) => merge(result, {
+    [keyFn(item)]: valueFn(item)
+  }), {})
+}
+
+function or<TValue> (optional: TValue | undefined, defaultValue: TValue): TValue {
+  return optional === undefined
+    ? defaultValue
+    : optional
+}
+
+function groupByCount<TKey extends string, TItem> (
+  arr: Array<TItem>,
+  keyFn: (item: TItem) => TKey
+): { [key in TKey]?: number } {
+  return arr.reduce((result: { [key in TKey]?: number }, item) => {
+    const key = keyFn(item)
+    const previousValue = result[key]
+    const newValue = or<number>(previousValue, 0) + 1
+    return merge(result, {
+      [key]: newValue + 1
+    })
+  }, {})
+}
+
+function mapToArray<TKey extends string, TValue> (map: { [key in TKey]?: TValue }) {
+  return Object.entries(map)
+}
+
+function get<TKey extends string, TValue> (obj: { [key in TKey]: TValue }, key: TKey): TValue {
+  return obj[key]
+}
+
+function getLatestReviews (pullRequestInfo: PullRequestInfo) {
   const sortedReviews = pullRequestInfo.reviews.nodes.sort(
     (a, b) =>
       new Date(a.submittedAt).getTime() - new Date(b.submittedAt).getTime()
@@ -130,65 +184,69 @@ function getPullRequestStatusFromReviews (
     sortedReviews
   )
 
-  const reviewSummary = Object.entries(latestReviewsByUser)
-    .map(([user, review]) => `${user}: ${review.state}`)
-    .join('\n')
-
-  log(`\nReviews:\n${reviewSummary}\n\n`)
-
   const latestReviews = Object.values(latestReviewsByUser)
-
-  let approvedByOneAssociation = false
-  for (let association of associations) {
-    const associationReviews = latestReviews.filter(review => getAssociationPriority(review.authorAssociation) >= getAssociationPriority(association))
-
-    const changesRequestedCount = associationReviews.filter(review => review.state === 'CHANGES_REQUESTED').length
-    const maxRequestedChanges = config.maxRequestedChanges[association]
-    if (maxRequestedChanges !== undefined && changesRequestedCount > maxRequestedChanges) {
-      return {
-        code: 'changes_requested',
-        message: `There are changes requested by a reviewer.`
-      }
-    }
-
-    const approvalCount = associationReviews.filter(review => review.state === 'APPROVED').length
-    const minApprovals = config.minApprovals[association]
-    if (minApprovals !== undefined && approvalCount >= minApprovals) {
-      approvedByOneAssociation = true
-    }
-  }
-
-  if (approvedByOneAssociation) {
-    return null
-  } else {
-    return {
-      code: 'need_approvals',
-      message: `There are not enough approvals by reviewers`
-    }
-  }
+  return latestReviews
 }
 
-function getPullRequestStatusFromChecks (
+function doesNotHaveMaximumChangesRequested (context: HandlerContext, pullRequestInfo: PullRequestInfo): ConditionResult {
+  const { config, log } = context
+
+  const latestReviews = getLatestReviews(pullRequestInfo)
+  const changesRequestedCountByAssociation =
+    arrayToMap(associations,
+      (association) => association,
+      (association) => latestReviews
+        .filter(review => getAssociationPriority(review.authorAssociation) >= getAssociationPriority(association))
+        .filter(review => review.state === 'CHANGES_REQUESTED')
+        .length
+      )
+
+  return mapToArray(config.maxRequestedChanges)
+    .some(([association, maxRequestedChanges]) => or(get(changesRequestedCountByAssociation, association), 0) > maxRequestedChanges)
+    ? {
+      status: 'fail',
+      message: `There are changes requested by a reviewer.`
+    }
+    : {
+      status: 'success'
+    }
+}
+
+function hasMinimumApprovals (context: HandlerContext, pullRequestInfo: PullRequestInfo): ConditionResult {
+  const { config } = context
+
+  const latestReviews = getLatestReviews(pullRequestInfo)
+  const approvalCountByAssociation =
+    arrayToMap(associations,
+      (association) => association,
+      (association) => latestReviews
+        .filter(review => getAssociationPriority(review.authorAssociation) >= getAssociationPriority(association))
+        .filter(review => review.state === 'APPROVED')
+        .length
+      )
+
+  return mapToArray(config.minApprovals)
+    .some(([association, minApproval]) => or(get(approvalCountByAssociation, association), 0) < minApproval)
+    ? {
+      status: 'fail',
+      message: 'There are not enough approvals by reviewers'
+    }
+    : {
+      status: 'success'
+    }
+}
+
+function doesNotHaveBlockingChecks (
   context: HandlerContext,
   pullRequestInfo: PullRequestInfo
-): PullRequestStatus | null {
-  const { log } = context
+): ConditionResult {
   const checkRuns = pullRequestInfo.checkRuns
-  // log('checks: ' + JSON.stringify(checks))
-  const checksSummary = checkRuns
-    .map(
-      checkRun => `${checkRun.name}: ${checkRun.status}: ${checkRun.conclusion}`
-    )
-    .join('\n')
-
-  log(`\nChecks:\n${checksSummary}\n\n`)
-
   const allChecksCompleted = checkRuns.every(
     checkRun => checkRun.status === 'completed'
   )
   if (!allChecksCompleted) {
     return {
-      code: 'pending_checks',
+      status: 'pending',
       message: 'There are still pending checks'
     }
   }
@@ -197,7 +255,6 @@ function getPullRequestStatusFromChecks (
     _ => true,
     checkRuns
   )
-  log('conclusions: ' + JSON.stringify(checkConclusions))
   const checksBlocking =
     checkConclusions.failure ||
     checkConclusions.cancelled ||
@@ -205,18 +262,19 @@ function getPullRequestStatusFromChecks (
     checkConclusions.action_required
   if (checksBlocking) {
     return {
-      code: 'blocking_check',
+      status: 'fail',
       message: 'There are blocking checks'
     }
   }
-
-  return null
+  return {
+    status: 'success'
+  }
 }
 
-function getPullRequestStatusFromProtectedBranch (
+function hasUpToDateBranch (
   context: HandlerContext,
   pullRequestInfo: PullRequestInfo
-): PullRequestStatus | null {
+): ConditionResult {
   const protectedBranch = pullRequestInfo.repository.protectedBranches.nodes
     .filter(protectedBranch => protectedBranch.name === pullRequestInfo.baseRef.name)[0]
 
@@ -224,7 +282,7 @@ function getPullRequestStatusFromProtectedBranch (
     && protectedBranch.hasStrictRequiredStatusChecks
     && pullRequestInfo.baseRef.target.oid !== pullRequestInfo.baseRefOid) {
     return {
-      code: 'out_of_date_branch',
+      status: 'fail',
       message: `Pull request is based on a strict protected branch (${
         pullRequestInfo.baseRef.name
       }) and base sha of pull request (${
@@ -233,21 +291,70 @@ function getPullRequestStatusFromProtectedBranch (
     }
   }
 
-  return null
+  return {
+    status: 'success'
+  }
+}
+
+function mapObject<TKey extends string, TValue, TMappedValue> (obj: { [key in TKey]: TValue }, mapper: (value: TValue) => TMappedValue): { [key in TKey]: TMappedValue } {
+  const result: any = {}
+  for (let key in obj) {
+    result[key] = mapper(obj[key])
+  }
+  return result
+}
+
+export function getConditionResults (
+  context: HandlerContext,
+  pullRequestInfo: PullRequestInfo
+): ConditionResults {
+  return mapObject<ConditionName, Condition, ConditionResult>(
+    conditions,
+    (condition: Condition) => condition(context, pullRequestInfo)
+  )
+}
+
+function isFail (conditionResult: ConditionResult): boolean {
+  return conditionResult.status === 'fail'
+}
+
+function getPullRequestStatusCode (results: ConditionResults): PullRequestStatusCode {
+  if (isFail(results.open)) {
+    return 'not_open'
+  } else if (isFail(results.mergeable)) {
+    return 'conflicts'
+  } else if (results.mergeable.status === 'pending') {
+    return 'pending_mergeable'
+  } else if (isFail(results.requiredLabels)) {
+    return 'requires_label'
+  } else if (isFail(results.blockingLabels)) {
+    return 'blocking_label'
+  } else if (isFail(results.maximumChangesRequested)) {
+    return 'changes_requested'
+  } else if (isFail(results.minimumApprovals)) {
+    return 'need_approvals'
+  } else if (isFail(results.blockingChecks)) {
+    return 'blocking_check'
+  } else if (results.blockingChecks.status === 'pending') {
+    return 'pending_checks'
+  } else if (isFail(results.upToDateBranch)) {
+    return 'out_of_date_branch'
+  } else {
+    return 'ready_for_merge'
+  }
 }
 
 export async function getPullRequestStatus (
   context: HandlerContext,
   pullRequestInfo: PullRequestInfo
 ): Promise<PullRequestStatus> {
-  return (
-    getPullRequestStatusFromPullRequest(context, pullRequestInfo) ||
-    getPullRequestStatusFromLabels(context, pullRequestInfo) ||
-    getPullRequestStatusFromReviews(context, pullRequestInfo) ||
-    getPullRequestStatusFromChecks(context, pullRequestInfo) ||
-    getPullRequestStatusFromProtectedBranch(context, pullRequestInfo) || {
-      code: 'ready_for_merge',
-      message: 'Pull request successfully merged'
-    }
-  )
+  const results = getConditionResults(context, pullRequestInfo)
+  return {
+    code: pullRequestInfo.state === 'CLOSED'
+    ? 'closed'
+    : pullRequestInfo.state === 'MERGED'
+    ? 'merged'
+    : getPullRequestStatusCode(results),
+    message: ''
+  }
 }
