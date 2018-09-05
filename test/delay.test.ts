8cd9f9d18bf3e391edc461c45d6b2a27cb9e45be
Bob van der Linden <bobvanderlinden@gmail.com>
2018-09-05 21:54:36 +0200

    test cancelling delays

--- test/delay.test.ts
+++ test/delay.test.ts
@@ -1,12 +1,38 @@
 jest.useFakeTimers()
 
-it('delay', async () => {
-  const { delay, immediate } = require('../src/delay')
-  const callback = jest.fn()
-  delay(1000).then(callback)
-  expect(setTimeout).toBeCalled()
-  expect(callback).not.toBeCalled()
-  jest.runAllTimers()
-  await immediate()
-  expect(callback).toBeCalled()
+describe('delay', () => {
+  it('resolves after specified amount of time', async () => {
+    const { delay, immediate } = require('../src/delay')
+    const callback = jest.fn()
+    delay(1000).then(callback)
+    await immediate()
+    expect(callback).not.toBeCalled()
+    jest.advanceTimersByTime(1000)
+    await immediate()
+    expect(callback).toBeCalled()
+  })
+
+  it('when cancelled, resolves immediately', async () => {
+    const { delay, immediate } = require('../src/delay')
+    const callback = jest.fn()
+    const promise = delay(1000)
+    promise.then(callback)
+    await immediate()
+    expect(callback).not.toBeCalled()
+    promise.cancel()
+    await immediate()
+    expect(callback).toBeCalled()
+  })
+})
+
+describe('immediate', () => {
+  it('resolves after tick', async () => {
+    const { immediate } = require('../src/delay')
+    const callback = jest.fn()
+    const promise = immediate().then(callback)
+    expect(setImmediate).toBeCalled()
+    expect(callback).not.toBeCalled()
+    await promise
+    expect(callback).toBeCalled()
+  })
 })
