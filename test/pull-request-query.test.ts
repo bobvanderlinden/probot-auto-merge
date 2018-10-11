1f236ec8a5b06bdb42384af78c534a1992d26c26
Bob van der Linden <bobvanderlinden@gmail.com>
2018-10-11 20:45:43 +0200

    add test for query without permissions error

--- test/pull-request-query.test.ts
+++ test/pull-request-query.test.ts
@@ -49,4 +49,21 @@ describe('queryPullRequest', () => {
       { owner: 'bobvanderlinden', repo: 'probot-auto-merge', number: 1 }
     )).rejects.toThrowError('Query result does not have repository')
   })
+
+  it('should throw error when no headRef and not mergeable', async () => {
+    const query = jest.fn(() => createPullRequestInfo({
+      repository: {
+        pullRequest: {
+          headRef: undefined,
+          mergeable: undefined
+        }
+      }
+    } as any))
+    expect(queryPullRequest(
+      createGithubApi({
+        query
+      }),
+      { owner: 'bobvanderlinden', repo: 'probot-auto-merge', number: 1 }
+    )).rejects.toThrowError('No permission to source repository of pull request')
+  })
 })
