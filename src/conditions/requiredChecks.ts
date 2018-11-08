56d7f99a382077f96e11a5463741b25516e59cd7
Bob van der Linden <bobvanderlinden@gmail.com>
2018-11-08 22:07:56 +0100

    fix requiredChecks to match tests

--- src/conditions/requiredChecks.ts
+++ src/conditions/requiredChecks.ts
@@ -13,7 +13,17 @@ function isPositiveCheckRun (checkRun: CheckRun): boolean {
     && positiveCheckRunConclusions.indexOf(checkRun.conclusion) > -1
 }
 
-export default function isOpen (
+function isPendingCheckRun (checkRun: CheckRun): boolean {
+  return checkRun.status !== 'completed'
+}
+
+function flatMap<TIn, TOut> (list: TIn[], fn: (item: TIn) => TOut[]): TOut[] {
+  return list.reduce((result: TOut[], item: TIn) => {
+    return result.concat(fn(item))
+  }, [])
+}
+
+export default function areRequiredChecksPositive (
   config: ConditionConfig,
   pullRequestInfo: PullRequestInfo
 ): ConditionResult {
@@ -21,22 +31,38 @@ export default function isOpen (
   const matchingBranchProtectionRules = branchProtectionRules
     .filter(rule => minimatch(pullRequestInfo.baseRef.name, rule.pattern))
 
-  const result = matchingBranchProtectionRules
-    .every(rule =>
+  const requiredStatusCheckContexts = new Set(
+    flatMap(matchingBranchProtectionRules, rule =>
       rule.requiredStatusCheckContexts
-        .every(requiredCheck => {
-          const checkRuns = pullRequestInfo.checkRuns
-          const matchingCheckRuns = checkRuns
-            .filter(checkRun => checkRun.name === requiredCheck)
-          return matchingCheckRuns.length > 0 && matchingCheckRuns.every(isPositiveCheckRun)
-        })
     )
-  return result
-    ? {
-      status: 'success'
+  )
+
+  const allRequiredChecksExist = Array.from(requiredStatusCheckContexts).every(requiredCheck => pullRequestInfo.checkRuns.some(checkRun => checkRun.name === requiredCheck))
+
+  if (!allRequiredChecksExist) {
+    return {
+      status: 'pending',
+      message: 'Required checks are missing'
     }
-    : {
+  }
+
+  const requiredCheckRuns = pullRequestInfo.checkRuns
+    .filter(checkRun => requiredStatusCheckContexts.has(checkRun.name))
+
+  const arePendingCheckRuns = requiredCheckRuns.some(isPendingCheckRun)
+  const allSucceedingCheckRuns = requiredCheckRuns.length === 0 || requiredCheckRuns.every(isPositiveCheckRun)
+
+  if (arePendingCheckRuns) {
+    return {
+      status: 'pending',
+      message: 'Waiting for required checks'
+    }
+  } else if (allSucceedingCheckRuns) {
+    return { status: 'success' }
+  } else {
+    return {
       status: 'fail',
-      message: `The required checks did not succeed for the protected branch`
+      message: `Required checks did not succeed for the protected branch`
     }
+  }
 }
