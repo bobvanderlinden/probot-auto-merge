2824679650986f7b1dc8ed7a7e5202b434525f3a
Bob van der Linden <bobvanderlinden@gmail.com>
2018-11-27 22:14:29 +0100

    remove __typename in query objects

--- src/github-models.ts
+++ src/github-models.ts
@@ -38,43 +38,74 @@ function assertNotNullNodes<TNode, TNodeOutput> (input: { nodes: (TNode | null)[
   }
 }
 
+function removeTypename<T extends { __typename: any }> (obj: T): Omit<T, '__typename'> {
+  const result = obj
+  delete result['__typename']
+  return result
+}
+
 export function validatePullRequestQuery (pullRequestQuery: PullRequestQuery) {
   return assertNotNull(pullRequestQuery, 'Could not query pull request',
     response => ({
       ...response,
       repository: assertNotNull(response.repository, 'Query result does not have repository',
         repository => ({
-          ...repository,
+          ...removeTypename(repository),
           pullRequest: assertNotNull(repository.pullRequest, 'No permission to source repository of pull request',
             pullRequest => ({
-              ...pullRequest,
+              ...removeTypename(pullRequest),
+              potentialMergeCommit: pullRequest.potentialMergeCommit && {
+                ...removeTypename(pullRequest.potentialMergeCommit),
+                oid: pullRequest.potentialMergeCommit.oid as string
+              },
               mergeable: assertNotNull(pullRequest.mergeable, 'No permission to source repository of pull request', mergeable => mergeable),
               labels: assertNotNullNodes(pullRequest.labels, 'No permission to labels of pull request',
                 labels => labels
               ),
               reviews: assertNotNullNodes(pullRequest.reviews, 'No permission to fetch reviews',
                 review => ({
-                  ...review,
-                  author: assertNotNull(review.author, 'No permission to fetch author of review', author => author)
+                  ...removeTypename(review),
+                  submittedAt: review.submittedAt as string,
+                  author: assertNotNull(review.author, 'No permission to fetch author of review', author => removeTypename(author))
                 })
               ),
               baseRef: assertNotNull(pullRequest.baseRef, 'No permission to fetch baseRef',
-                baseRef => baseRef
+                baseRef => ({
+                  ...removeTypename(baseRef),
+                  repository: {
+                    ...removeTypename(baseRef.repository),
+                    owner: removeTypename(baseRef.repository.owner)
+                  },
+                  target: {
+                    oid: baseRef.target.oid as string
+                  }
+                })
               ),
+              baseRefOid: pullRequest.baseRefOid as string,
               headRef: assertNotNull(pullRequest.headRef, 'No permission to fetch headRef',
-                headRef => headRef
+                headRef => ({
+                  ...removeTypename(headRef),
+                  repository: {
+                    ...removeTypename(headRef.repository),
+                    owner: removeTypename(headRef.repository.owner)
+                  },
+                  target: {
+                    oid: headRef.target.oid as string
+                  }
+                })
               ),
+              headRefOid: pullRequest.headRefOid as string,
               commits: assertNotNullNodes(pullRequest.commits, 'No permission to fetch commits',
                 commit => ({
-                  ...commit,
+                  ...removeTypename(commit),
                   commit: {
-                    ...commit.commit,
+                    ...removeTypename(commit.commit),
                     checkSuites: assertNotNullNodes(commit.commit.checkSuites, 'No permission to fetch checkSuites',
                       checkSuite => ({
-                        ...checkSuite,
-                        app: assertNotNull(checkSuite.app, 'No permission to fetch app', app => app),
+                        ...removeTypename(checkSuite),
+                        app: assertNotNull(checkSuite.app, 'No permission to fetch app', app => removeTypename(app)),
                         checkRuns: assertNotNullNodes(checkSuite.checkRuns, 'No permission to fetch checkRuns',
-                          checkRun => checkRun
+                          checkRun => removeTypename(checkRun)
                         )
                       })
                     )
@@ -82,10 +113,10 @@ export function validatePullRequestQuery (pullRequestQuery: PullRequestQuery) {
                 })
               ),
               repository: {
-                ...pullRequest.repository,
+                ...removeTypename(pullRequest.repository),
                 branchProtectionRules: assertNotNullNodes(pullRequest.repository.branchProtectionRules, 'No permission to fetch branchProtectionRules',
                   branchProtectionRule => ({
-                    ...branchProtectionRule,
+                    ...removeTypename(branchProtectionRule),
                     requiredStatusCheckContexts: assertNotNull(branchProtectionRule.requiredStatusCheckContexts, 'No permission to fetch requiredStatusCheckContexts',
                       requiredStatusCheckContexts => requiredStatusCheckContexts.map(requiredStatusCheckContext => assertNotNull(requiredStatusCheckContext, 'No permission to fetch requiredStatusCheckContext', requiredStatusCheckContext => requiredStatusCheckContext))
                     )
