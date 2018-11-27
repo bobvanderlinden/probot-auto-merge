fd8c280b412ce223e1effbe573825d80ecf58a50
Bob van der Linden <bobvanderlinden@gmail.com>
2018-11-27 22:17:07 +0100

    removed commented DeepOmit

--- src/type-utils.ts
+++ src/type-utils.ts
@@ -10,42 +10,6 @@ export type ReturnType<T extends AnyFunction> = T extends (...args: any[]) => in
 export type Diff<T, U> = T extends U ? never : T
 export type NonNullable<T> = Diff<T, null | undefined>
 
-// type DeepOmit<T, TOmitKey> = {
-//   'nullable': DeepOmit<Exclude<T, null>, TOmitKey> | null,
-//   'primitive': T,
-//   'array': T extends Array<infer TSubType>
-//     ? Array<DeepOmit<TSubType, TOmitKey>>
-//     : never,
-//   'object': TOmitKey extends keyof T
-//     ? { [TKey in Exclude<keyof T, TOmitKey>]: DeepOmit<T[TKey], TOmitKey> }
-//     : { [TKey in keyof T]: DeepOmit<T[TKey], TOmitKey> },
-//   'default': T
-// }[T extends Primitive
-//   ? 'primitive'
-//   : T extends Array<any>
-//   ? 'array'
-//   : T extends object
-//   ? 'object'
-//   : 'default'
-// ]
-
-// export type DeepOmit<T, TOmitKey> = TOmitKey extends keyof T
-//   ? { [TKey in Exclude<keyof T, TOmitKey>]: DeepOmit<T[TKey], TOmitKey> }
-//   : T extends infer TA | infer TB
-//   ? DeepOmitFromUnion<TA, TB, TKey>
-//   : T extends Array<infer TItem>
-//   ? DeepOmitFromArray<TItem, TOmitKey>
-//   : T extends Primitive
-//   ? T
-//   : T extends object
-//   ? DeepOmitFromObject<T, TOmitKey>
-//   : never
-// export interface DeepOmitFromArray<T, TOmitKey> extends Array<DeepOmit<T, TOmitKey>> {}
-// export type DeepOmitFromObject<T, TOmitKey> = {
-//   [TKey in keyof T]: DeepOmit<T[TKey], TOmitKey>;
-// }
-// export type DeepOmitFromUnion<TA, TB, TKey> = { 'A': DeepOmit<TA, TKey> | DeepOmit<TB, TKey> }[TAap]
-
 export type Convert<T, TMatch, TConvert> = T extends TMatch ? TConvert : T
 export type DeepConvert<T, TMatch, TConvert> = T extends TMatch
   ? TConvert
