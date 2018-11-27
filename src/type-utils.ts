c8e105758c0bf20b816947897960cc880540bf52
Bob van der Linden <bobvanderlinden@gmail.com>
2018-11-27 22:29:15 +0100

    remove unused type definitions from type-utils

--- src/type-utils.ts
+++ src/type-utils.ts
@@ -9,20 +9,3 @@ export type ReturnType<T extends AnyFunction> = T extends (...args: any[]) => in
 
 export type Diff<T, U> = T extends U ? never : T
 export type NonNullable<T> = Diff<T, null | undefined>
-
-export type Convert<T, TMatch, TConvert> = T extends TMatch ? TConvert : T
-export type DeepConvert<T, TMatch, TConvert> = T extends TMatch
-  ? TConvert
-  : T extends Primitive
-  ? T
-  : T extends Array<infer TItem>
-  ? DeepConvertFromArray<TItem, TMatch, TConvert>
-  : T extends { [key: string]: any }
-  ? DeepConvertFromObject<T, TMatch, TConvert>
-  : never
-export interface DeepConvertFromArray<T, TMatch, TConvert> extends Array<DeepConvert<T, TMatch, TConvert>> { }
-export type DeepConvertFromObject<T, TMatch, TConvert> = {
-  [TKey in keyof T]: DeepConvert<T[TKey], TMatch, TConvert>;
-}
-
-export type ReplaceFields<T, TKey extends keyof T, TReplacementType extends { [Key in TKey]: any }> = Omit<T, TKey> & TReplacementType
