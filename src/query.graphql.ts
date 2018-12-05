00e831438a18956e7283539b889386cd42ac74b6
Bob van der Linden <bobvanderlinden@gmail.com>
2018-12-05 20:59:09 +0100

    move __generated__ to src/query.graphql.ts

--- src/query.graphql.ts
+++ src/query.graphql.ts
@@ -1,8 +1,6 @@
 /* tslint:disable */
 // This file was automatically generated and should not be edited.
 
-import { PullRequestState, MergeableState, CommentAuthorAssociation, PullRequestReviewState, CheckStatusState, CheckConclusionState } from "./globalTypes";
-
 // ====================================================
 // GraphQL query operation: PullRequestQuery
 // ====================================================
@@ -356,3 +354,78 @@ export interface PullRequestQueryVariables {
   repo: string;
   pullRequestNumber: number;
 }
+
+/* tslint:disable */
+// This file was automatically generated and should not be edited.
+
+//==============================================================
+// START Enums and Input Objects
+//==============================================================
+
+/**
+ * The possible states for a check suite or run conclusion.
+ */
+export enum CheckConclusionState {
+  ACTION_REQUIRED = "ACTION_REQUIRED",
+  CANCELLED = "CANCELLED",
+  FAILURE = "FAILURE",
+  NEUTRAL = "NEUTRAL",
+  SUCCESS = "SUCCESS",
+  TIMED_OUT = "TIMED_OUT",
+}
+
+/**
+ * The possible states for a check suite or run status.
+ */
+export enum CheckStatusState {
+  COMPLETED = "COMPLETED",
+  IN_PROGRESS = "IN_PROGRESS",
+  QUEUED = "QUEUED",
+  REQUESTED = "REQUESTED",
+}
+
+/**
+ * A comment author association with repository.
+ */
+export enum CommentAuthorAssociation {
+  COLLABORATOR = "COLLABORATOR",
+  CONTRIBUTOR = "CONTRIBUTOR",
+  FIRST_TIMER = "FIRST_TIMER",
+  FIRST_TIME_CONTRIBUTOR = "FIRST_TIME_CONTRIBUTOR",
+  MEMBER = "MEMBER",
+  NONE = "NONE",
+  OWNER = "OWNER",
+}
+
+/**
+ * Whether or not a PullRequest can be merged.
+ */
+export enum MergeableState {
+  CONFLICTING = "CONFLICTING",
+  MERGEABLE = "MERGEABLE",
+  UNKNOWN = "UNKNOWN",
+}
+
+/**
+ * The possible states of a pull request review.
+ */
+export enum PullRequestReviewState {
+  APPROVED = "APPROVED",
+  CHANGES_REQUESTED = "CHANGES_REQUESTED",
+  COMMENTED = "COMMENTED",
+  DISMISSED = "DISMISSED",
+  PENDING = "PENDING",
+}
+
+/**
+ * The possible states of a pull request.
+ */
+export enum PullRequestState {
+  CLOSED = "CLOSED",
+  MERGED = "MERGED",
+  OPEN = "OPEN",
+}
+
+//==============================================================
+// END Enums and Input Objects
+//==============================================================
