cff492ad6c0eccf180d5507b1177fda54e4e82c3
Bob van der Linden <bobvanderlinden@gmail.com>
2019-10-16 10:35:29 +0200

    Revert "Merge pull request #381 from bobvanderlinden/pr-required-status-checks"
    This reverts commit cb2483ed05450b4651f9e4b2892b1a0d38decf9a, reversing
    changes made to 6f880b5fb1a4a3f1b1dd52f5d410eb84b5071b1c.

--- src/conditions/requiredChecks.ts
+++ src/conditions/requiredChecks.ts
@@ -1,8 +1,23 @@
+import { CheckRun } from './../github-models'
 import { ConditionConfig } from './../config'
 import { PullRequestInfo } from '../models'
 import { ConditionResult } from '../condition'
 import minimatch from 'minimatch'
-import { StatusState } from '../query.graphql'
+import { CheckConclusionState, CheckStatusState } from '../github-models'
+import { getOtherCheckRuns } from '../utils'
+
+const positiveCheckRunConclusions: Array<CheckRun['conclusion']> = [
+  CheckConclusionState.SUCCESS, CheckConclusionState.NEUTRAL
+]
+
+function isPositiveCheckRun (checkRun: CheckRun): boolean {
+  return checkRun.status === CheckStatusState.COMPLETED
+    && positiveCheckRunConclusions.indexOf(checkRun.conclusion) > -1
+}
+
+function isPendingCheckRun (checkRun: CheckRun): boolean {
+  return checkRun.status !== CheckStatusState.COMPLETED
+}
 
 function flatMap<TIn, TOut> (list: TIn[], fn: (item: TIn) => TOut[]): TOut[] {
   return list.reduce((result: TOut[], item: TIn) => {
@@ -18,32 +33,16 @@ export default function areRequiredChecksPositive (
   const matchingBranchProtectionRules = branchProtectionRules
     .filter(rule => minimatch(pullRequestInfo.baseRef.name, rule.pattern))
 
-  const requiredStatusCheckContextNames = new Set(
+  const requiredStatusCheckContexts = new Set(
     flatMap(matchingBranchProtectionRules, rule =>
       rule.requiredStatusCheckContexts
     )
   )
 
-  if (requiredStatusCheckContextNames.size === 0) {
-    // There are no required status checks, so this condition passes.
-    return { status: 'success' }
-  }
-
-  const head = pullRequestInfo.commits.nodes[0]
-
-  if (!head) {
-    return {
-      status: 'fail',
-      message: 'Could not fetch any commit, but there are required checks'
-    }
-  }
-
-  const requiredStatusCheckContexts = Array.from(requiredStatusCheckContextNames)
-    .map(requiredStatusCheckContextName =>
-      head.commit.status.contexts
-        .filter(context => context.context === requiredStatusCheckContextName)[0]
+  const allRequiredChecksExist = Array.from(requiredStatusCheckContexts)
+    .every(requiredCheck => getOtherCheckRuns(pullRequestInfo)
+      .some(checkRun => checkRun.name === requiredCheck)
     )
-  const allRequiredChecksExist = requiredStatusCheckContexts.every(requiredStatusCheckContext => requiredStatusCheckContext)
 
   if (!allRequiredChecksExist) {
     return {
@@ -52,8 +51,11 @@ export default function areRequiredChecksPositive (
     }
   }
 
-  const arePendingCheckRuns = requiredStatusCheckContexts.some(statusCheck => statusCheck.state === StatusState.PENDING)
-  const allSucceedingCheckRuns = requiredStatusCheckContexts.every(statusCheck => statusCheck.state === StatusState.SUCCESS)
+  const requiredCheckRuns = getOtherCheckRuns(pullRequestInfo)
+    .filter(checkRun => requiredStatusCheckContexts.has(checkRun.name))
+
+  const arePendingCheckRuns = requiredCheckRuns.some(isPendingCheckRun)
+  const allSucceedingCheckRuns = requiredCheckRuns.length === 0 || requiredCheckRuns.every(isPositiveCheckRun)
 
   if (arePendingCheckRuns) {
     return {
