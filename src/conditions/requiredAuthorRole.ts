dbfb199063400093586c721a715515757328c39c
Bob van der Linden <bobvanderlinden@gmail.com>
2020-05-24 17:47:54 +0200

    reuse getAssociationPriority for requiredAuthorRole

--- src/conditions/requiredAuthorRole.ts
+++ src/conditions/requiredAuthorRole.ts
@@ -7,9 +7,7 @@ export default function hasRequiredAuthorRole (
   config: ConditionConfig,
   pullRequestInfo: PullRequestInfo
 ): ConditionResult {
-  const associationString = ['NONE', 'FIRST_TIME', 'FIRST_TIME_CONTRIBUTOR', 'CONTRIBUTOR', 'COLLABORATOR', 'MEMBER', 'OWNER']
-
-  return associationString.indexOf(config.requiredAuthorRole) > getAssociationPriority(pullRequestInfo.authorAssociation)
+  return getAssociationPriority(config.requiredAuthorRole) > getAssociationPriority(pullRequestInfo.authorAssociation)
     ? {
       status: 'fail',
       message: 'The author of the pull request does not have authority for auto-merge'
